1f3075998ae284c5938d47dfb181b94ef2f01810
Umuras <49074939+Umuras@users.noreply.github.com>
2020-12-27 18:03:44 +0300

    KodluyoruzÖdevler

---
+++ Kodluyoruz3.Odev/Assets/Scripts/Controller/PauseControl.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+     
+    void Update()
+    {
+        
+    }
+
+    public void ResumeButton()
+    {
+        SceneManager.UnloadSceneAsync("PauseMenu");
+    }
+
+    public void RestartButton()
+    {
+        SceneManager.LoadScene("SampleScene");
+        
+    }
+
+    public void MainMenuButton()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
